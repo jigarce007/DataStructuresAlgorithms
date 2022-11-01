8f1843b05ce73f7aec74ba759be3982b669ea348
80006001 <jigararvindbhai.maybank.com>
2022-11-01 21:36:56 +0530

    first commit

---
+++ AddDigits.java
@@ -0,0 +1,17 @@
+public class AddDigits
+ {
+public static void main(String[] args) {
+    System.out.println("Hello World" +addDigits(123456789));
+}    
+static int addDigits(int n ){
+    int sum;
+    if(n<0){
+        return 0;
+    }
+
+    if(n<10){return n;}
+
+    sum = n%10 + addDigits(n/10);
+    return sum;
+}
+}
