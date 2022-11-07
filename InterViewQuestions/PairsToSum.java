10fdfbb8c62cb69d457df75eda067e637cbd6252
80006001 <jigararvindbhai.maybank.com>
2022-11-08 00:02:19 +0530

    Updated code formate

--- InterViewQuestions/PairsToSum.java
+++ InterViewQuestions/PairsToSum.java
@@ -14,30 +14,35 @@ Output: Because nums[0] + nums[1] == 9, we return [0, 1]
  */
 
 package InterViewQuestions;
+
+import java.util.Arrays;
+
 public class PairsToSum
  {
 public static void main(String[] args) {
     int[] intAry = {1,2,6,4,8,5,1,9,5};
-    //System.out.println("Pair is = " +Arrays.toString(twoSum(intAry,10)));
-    int[] temp = twoSum(intAry,10);
+     twoSum(intAry,10);
+    
 }    
 
-public static int[] twoSum(int[] nums, int target) {
+
+public static void twoSum(int[] nums, int target) {
     // TODO
     int temp =0;
-    int[] result = new int[nums.length];
+    int[] result = new int[2];
     for(int i=0;i<nums.length;i++){         //Time complexity = O(n)
         for(int j=i+1;j<nums.length;j++){  //Time complexity = O(n)
              temp = nums[i]+nums[j];         //Time comp = O(1)
         
             if(temp == target){
-                result[i] = i+1; //Time comp = O(1)
-                result[j] =j+1;//Time comp = O(1)
-                System.out.println("Pair is = ["+i+ ","+j+"]");//Time comp = O(1)
+                result[0] = i; //Time comp = O(1)
+                result[1] =j;//Time comp = O(1)
+                System.out.println("Pair is = " +Arrays.toString(result));//Time comp = O(1)
+               
             }
         }
     }
-    
-    return result;
+   
 }
-}
+ }
+
