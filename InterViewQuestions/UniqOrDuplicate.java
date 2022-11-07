d801a280eb7b1cc7064d108d1814efb051244d42
80006001 <jigararvindbhai.maybank.com>
2022-11-08 01:34:54 +0530

    Added for checking duplicate characters in string

--- InterViewQuestions/UniqOrDuplicate.java
+++ InterViewQuestions/UniqOrDuplicate.java
@@ -18,22 +18,40 @@ public class UniqOrDuplicate
  {
 public static void main(String[] args) {
     int[] ary = {1,4,12,6}; 
+    String input = "Jigar";
     System.out.println("Array contains Uniq elements? = "+isUniq(ary));
+
+    //for string contains duplicate
+    System.out.println("String contains uniq characters? = "+isUniq(input));
+
     
 }    
 static boolean isUniq(int[] ary){
-    boolean flag = false;
+    //boolean flag = false;
     for (int i = 0; i < ary.length; i++) {
         for (int j = i+1; j < ary.length; j++) {
             if(ary[i] == ary[j]){
-               flag = true;
-                break;
-          
+              return false;
             }
         }
         
     }
    
-   return flag;
+   return true;
+}
+
+
+//For String Array
+static boolean isUniq(String input){
+    //boolean flag = false;
+    for (int i = 0; i < input.length(); i++) {
+        for (int j = i+1; j < input.length(); j++) {
+            if(input.charAt(i) == input.charAt(j)){
+              return false;
+            }
+        }
+        
+    }
+   return true;
 }
 }
